3026b1295b4702b8e600ff02b394f3356aeea399
Alexander Hernandez <bestrafermich@gmail.com>
2019-11-29 07:15:28 -0800

    Added links to navbar and checks for logged in user

--- navy_blog/urls.py
+++ navy_blog/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', accounts_views.signup, name='signup'),
     path("login/", auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path("logout/", auth_views.LogoutView.as_view(), name='logout'),
     path("", include("blog.urls")),
 ]
